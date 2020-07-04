7cb018714efd292257a402ddc4d5a3fd2d94a59d
Aleksei Arro <polsck@gmail.com>
2020-07-04 18:06:14 +0300

    Update core ESLint version

--- partials/main.js
+++ partials/main.js
@@ -33,6 +33,7 @@ const baseRules = {
     'error',
     {
       ignoreDestructuring: false,
+      ignoreGlobals: true,
       ignoreImports: false,
       properties: 'never',
     },
@@ -124,7 +125,7 @@ const baseRules = {
   ],
   'grouped-accessor-pairs': ['error', 'getBeforeSet'],
   'guard-for-in': 'error',
-  'id-blacklist': 'off',
+  'id-denylist': 'off',
   'id-length': 'off',
   'id-match': 'off',
   'implicit-arrow-linebreak': ['error', 'beside'],
