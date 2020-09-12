100417539eb433960801cac95ada2a43d6f07182
Aleksei Arro <polsck@gmail.com>
2020-09-12 17:35:43 +0300

    Add correct prefix to Babel plugin rules

--- partials/babel.js
+++ partials/babel.js
@@ -4,11 +4,11 @@ const { babelExtensibleRules } = require('./main');
 const { getDisabledRuleSet } = require('./utils');
 
 const enabledRules = {
-  'babel/new-cap': babelExtensibleRules['new-cap'],
-  'babel/no-invalid-this': babelExtensibleRules['no-invalid-this'],
-  'babel/no-unused-expressions': babelExtensibleRules['no-unused-expressions'],
-  'babel/object-curly-spacing': babelExtensibleRules['object-curly-spacing'],
-  'babel/semi': babelExtensibleRules.semi,
+  '@babel/new-cap': babelExtensibleRules['new-cap'],
+  '@babel/no-invalid-this': babelExtensibleRules['no-invalid-this'],
+  '@babel/no-unused-expressions': babelExtensibleRules['no-unused-expressions'],
+  '@babel/object-curly-spacing': babelExtensibleRules['object-curly-spacing'],
+  '@babel/semi': babelExtensibleRules.semi,
 };
 
 const disabledRules = getDisabledRuleSet(enabledRules);
