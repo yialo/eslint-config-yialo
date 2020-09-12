b9e09c32b58a6a69e30f96ab2218039fc252646f
Aleksei Arro <polsck@gmail.com>
2020-09-12 17:28:40 +0300

    Remove accomplished TODO

--- mixins/babel.js
+++ mixins/babel.js
@@ -3,12 +3,6 @@
 const { enabledRules: enabledBabelRules } = require('../partials/babel');
 const { babelResetRules: babelResetMainRules } = require('../partials/main');
 
-/**
- * TODO:
- * Update this config and README according to documentation:
- * @see {@link https://www.npmjs.com/package/@babel/eslint-parser}
- */
-
 module.exports = {
   parser: '@babel/eslint-parser',
   parserOptions: {
