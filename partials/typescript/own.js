3666dc74f318ba0ade807cab3e25dc0098b8a67c
Aleksei Arro <polsck@gmail.com>
2021-02-17 13:14:53 +0300

    Disable @typescript-eslint/no-non-null-assertion

--- partials/typescript/own.js
+++ partials/typescript/own.js
@@ -40,7 +40,7 @@ const regularOwnTsRules = {
   '@typescript-eslint/no-misused-new': 'error',
   '@typescript-eslint/no-namespace': 'error',
   '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
-  '@typescript-eslint/no-non-null-assertion': 'warn',
+  '@typescript-eslint/no-non-null-assertion': 'off',
   '@typescript-eslint/no-parameter-properties': 'off',
   '@typescript-eslint/no-require-imports': 'off',
   '@typescript-eslint/no-this-alias': 'error',
