87f0355aecb08a51ad611784db609af320077b07
Aleksei Arro <polsck@gmail.com>
2021-09-19 15:59:47 +0300

    refactor: add extplicit setting to TS parser

--- mixins/ts-nocheck.js
+++ mixins/ts-nocheck.js
@@ -6,6 +6,9 @@ const { tsRules_nonTypeCheck } = require('../partials/typescript');
 
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    warnOnUnsupportedTypeScriptVersion: true,
+  },
   rules: {
     ...coreRules_extensibleWithTs_nonTypeCheck_OFF,
     ...tsRules_nonTypeCheck,
