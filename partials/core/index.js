acadd50bbf88ab976dff94f6bcf5fa272d60abe7
Aleksei Arro <polsck@gmail.com>
2021-09-24 16:58:34 +0300

    refactor: add core/index export defs

--- partials/core/index.js
+++ partials/core/index.js
@@ -15,13 +15,57 @@ const {
 const { coreRules_nonExtensible } = require('./non-extensible');
 
 module.exports = {
+  /**
+   * Usage:
+   * - For full core ruleset composition in abstract config mixin
+   */
   coreRules_nonExtensible,
+
+  /**
+   * Usage:
+   * - For full core ruleset composition in abstract config mixin
+   */
   coreRules_extensibleShared,
+
+  /**
+   * Usage:
+   * - As reference to core rules for extending in Babel plugin partial
+   */
   coreRules_extensibleWithBabel_full,
+
+  /**
+   * Usage:
+   * - Disables all Babel-extensible core rules in Babel config mixin
+   */
   coreRules_extensibleWithBabel_full_OFF,
+
+  /**
+   * Usage:
+   * - For full core ruleset composition in abstract config mixin
+   */
   coreRules_extensibleWithBabel_only,
+
+  /**
+   * Usage:
+   * - As reference to core rules for extending in TS plugin partial
+   */
   coreRules_extensibleWithTs_full,
+
+  /**
+   * Usage:
+   * - For full core ruleset composition in abstract config mixin
+   */
   coreRules_extensibleWithTs_only,
+
+  /**
+   * Usage:
+   * - Disables all non-typecheck TS-extensible core rules in non-typecheck TS config mixin
+   */
   coreRules_extensibleWithTs_nonTypeCheck_OFF,
+
+  /**
+   * Usage:
+   * - Disables all typecheck-only TS-extensible core rules in typecheck-only TS config mixin
+   */
   coreRules_extensibleWithTs_typeCheckOnly_OFF,
 };
