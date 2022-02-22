090eb4eb9ad71a627f7fdd97a2a2139a1f66054b
Aleksei Arro <polsck@gmail.com>
2022-02-22 17:52:51 +0300

    refactor: add getDeprecatedReferenceRuleNames util

--- fixtures/compare-rules/utils.js
+++ fixtures/compare-rules/utils.js
@@ -4,17 +4,31 @@ module.exports.compareRuleLists = (
   { myRuleNames, referenceRuleNames, deprecatedRuleNames = [] },
   { pluginName },
 ) => {
-  const nonDeprecatedReferenceRuleNames = referenceRuleNames
-    .filter((ruleName) => !deprecatedRuleNames.includes(ruleName));
+  const nonDeprecatedReferenceRuleNames = referenceRuleNames.filter(
+    (ruleName) => !deprecatedRuleNames.includes(ruleName),
+  );
 
-  const missingRuleNames = nonDeprecatedReferenceRuleNames
-    .filter((ruleName) => !myRuleNames.includes(ruleName));
+  const missingRuleNames = nonDeprecatedReferenceRuleNames.filter(
+    (ruleName) => !myRuleNames.includes(ruleName),
+  );
 
-  const extraneousRuleNames = myRuleNames
-    .filter((ruleName) => !nonDeprecatedReferenceRuleNames.includes(ruleName));
+  const extraneousRuleNames = myRuleNames.filter(
+    (ruleName) => !nonDeprecatedReferenceRuleNames.includes(ruleName),
+  );
 
   console.group(pluginName);
   console.log('missingRuleNames', missingRuleNames.length, missingRuleNames);
   console.log('extraneousRuleNames', extraneousRuleNames.length, extraneousRuleNames);
   console.groupEnd(pluginName);
+};
+
+module.exports.getDeprecatedReferenceRuleNames = (referenceRuleEntries, ruleNamePrefix = '') => {
+  const normalizedPrefix = ruleNamePrefix && `${ruleNamePrefix}/`;
+
+  return referenceRuleEntries
+    .filter((ruleEntry) => {
+      const rule = ruleEntry[1];
+      return rule.meta.deprecated;
+    })
+    .map(([ruleName]) => `${normalizedPrefix}${ruleName}`);
 };
