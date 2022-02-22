26f3e0b539ba1c38e298aaf382fce6a64a1eeaf4
Aleksei Arro <polsck@gmail.com>
2022-02-22 19:17:55 +0300

    feat: add missing rule to react ruleset

--- partials/react.js
+++ partials/react.js
@@ -179,6 +179,7 @@ const reactRules = {
   'react/no-unescaped-entities': 'error',
   'react/no-unknown-property': 'error',
   'react/no-unsafe': 'error',
+  'react/no-unstable-nested-components': 'off',
   'react/no-unused-class-component-methods': 'warn',
   'react/no-unused-prop-types': 'off',
   'react/no-unused-state': 'error',
