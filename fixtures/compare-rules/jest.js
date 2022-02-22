ef32c6d0472adfc44390c46f37f068e86f26a93e
Aleksei Arro <polsck@gmail.com>
2022-02-22 14:44:06 +0300

    test: add compare-rules util for Jest

---
+++ fixtures/compare-rules/jest.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { rules: referenceJestRules } = require('eslint-plugin-jest');
+
+const { jestRules, jestTsRules_typeCheckOnly } = require('../../partials/jest');
+const { compareRuleLists } = require('./utils');
+
+const referenceJestRuleNames = Object.keys(referenceJestRules)
+  .map((ruleName) => `jest/${ruleName}`);
+
+const deprecaredReferenceJestRuleNames = Object.entries(referenceJestRules)
+  .filter((ruleEntry) => {
+    const rule = ruleEntry[1];
+    return rule.meta.deprecated;
+  })
+  .map(([ruleName]) => `jest/${ruleName}`);
+
+const myJestRuleNames = Object.keys({
+  ...jestRules,
+  ...jestTsRules_typeCheckOnly,
+});
+
+compareRuleLists({
+  deprecatedRuleNames: deprecaredReferenceJestRuleNames,
+  myRuleNames: myJestRuleNames,
+  referenceRuleNames: referenceJestRuleNames,
+}, 'jest');
