f174c7902212dae8e99faf094adddd7eed55a10c
Aleksei Arro <polsck@gmail.com>
2023-03-25 22:19:57 +0400

    refactor: disable all rule comparators but core

--- scripts/compare-rules/index.js
+++ scripts/compare-rules/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-void require('./babel');
-void require('./core');
-void require('./import');
-void require('./jest');
-void require('./jsx-a11y');
-void require('./node');
-void require('./promise');
-void require('./react-hooks');
-void require('./react');
-void require('./typescript');
+// void require('./babel');
+void require('./core_new');
+// void require('./import');
+// void require('./jest');
+// void require('./jsx-a11y');
+// void require('./node');
+// void require('./promise');
+// void require('./react-hooks');
+// void require('./react');
+// void require('./typescript');
