c6d4c8d48c9c1d32f45abbd1827eb52c0cc96f9a
Aleksei Arro <polsck@gmail.com>
2023-04-12 18:44:49 +0400

    feat: update TS plugin and reconfigure rules
    New TS plugin version: 5.58.0

--- src/partials/typescript/own.js
+++ src/partials/typescript/own.js
@@ -159,6 +159,7 @@ const tsRules_own_typeCheckOnly_nonExtensible = {
   '@typescript-eslint/no-unsafe-argument': 'error',
   '@typescript-eslint/no-unsafe-assignment': 'error',
   '@typescript-eslint/no-unsafe-call': 'error',
+  '@typescript-eslint/no-unsafe-enum-comparison': 'error',
   '@typescript-eslint/no-unsafe-member-access': 'error',
   '@typescript-eslint/no-unsafe-return': 'error',
   '@typescript-eslint/prefer-includes': 'off',
